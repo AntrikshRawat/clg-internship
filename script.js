9aa22b7da6f06958b5905d5e8111bf1935baea98
Antriksh rawat <antrikshrawat2@gmail.com>
2024-08-14 21:24:16 +0530

    by AR

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ let login = document.querySelector(".login");
 login.addEventListener("click", () => {
   window.location.assign("Login.html");
 });
+let anchor = document.querySelectorAll("a");
+anchor.forEach((a)=>{
+  a.addEventListener("click",(e)=>{
+    e.preventDefault();
+  })
+})
