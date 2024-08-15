5a356bfcb5aabf4633be6031a0f03a62d1d713f9
Bhavya Jain <jainbhavya2306@gmail.com>
2024-08-15 23:29:34 +0530

    fixFooter

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ login.addEventListener("click", () => {
   window.location.assign("Login.html");
 });
 let anchor = document.querySelectorAll("a");
-anchor.forEach((a)=>{
-  a.addEventListener("click",(e)=>{
+anchor.forEach((a) => {
+  a.addEventListener("click", (e) => {
     e.preventDefault();
-  })
-})
+  });
+});
