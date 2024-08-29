1c1b088b6831be11f40eb4d48444aedc5570b5b9
Antriksh rawat <antrikshrawat2@gmail.com>
2024-08-29 14:23:19 +0530

    by AR

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-let login = document.querySelector(".login");
+if(window.location.pathname === "/index.html"){
+  let login = document.querySelector(".login");
 login.addEventListener("click", () => {
   window.location.assign("Login.html");
 });
@@ -8,3 +9,28 @@ anchor.forEach((a) => {
     e.preventDefault();
   });
 });
+
+let createRoom = document.querySelectorAll("#create");
+createRoom.forEach((btn)=>{
+  btn.addEventListener('click',()=>{
+    window.location.pathname = "/createroom.html";
+  })
+});
+
+let joinRoom = document.querySelectorAll("#join");
+joinRoom.forEach((btn)=>{
+  btn.addEventListener('click',()=>{
+    window.location.pathname = "/joinroom.html";
+  })
+})
+}
+if(window.location.pathname === "/joinroom.html") {
+  let copyToClip = document.querySelector('.copy');
+  let input = document.querySelector('input');
+  let roomCode = input.value;
+  copyToClip.addEventListener('click',()=>{
+    navigator.clipboard.writeText(roomCode).then(()=>{
+      alert("Room Code Copied To ClipBoard");
+    })
+  })
+}
